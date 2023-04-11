6454ca7530e298411c35fec04dfc54bb0b22b1ab
Duy Hao Nguyen <duyhao301202@gmail.com>
2023-04-11 09:54:54 +0700

    project initiation

---
+++ ProjectRegistration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectRegistration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
