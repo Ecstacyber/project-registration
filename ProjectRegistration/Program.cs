0cf29e6c7f30f9465220c057ebbb00ac367b53fc
vsinh <20520737@gm.uit.edu.vn>
2023-05-04 13:25:21 +0700

    Simple web of User, Department, Course

--- ProjectRegistration/Program.cs
+++ ProjectRegistration/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ProjectRegistration.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ProjectRegistrationManagementContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
