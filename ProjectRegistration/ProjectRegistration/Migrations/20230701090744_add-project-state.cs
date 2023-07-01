205e050a85138d60ce2ef1fb4e2035bca8802e26
Duy Hao Nguyen <duyhao301202@gmail.com>
2023-07-01 16:08:41 +0700

    add-project-state migration

---
+++ ProjectRegistration/ProjectRegistration/Migrations/20230701090744_add-project-state.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectRegistration.Migrations
+{
+    public partial class addprojectstate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "PGrade",
+                table: "Projects",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PGrade",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Projects");
+        }
+    }
+}
